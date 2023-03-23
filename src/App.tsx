8d87dc081e6730586b7da5abdbec2ea245f37c21
ItsRau1 <00001085823507sp@al.educacao.sp.gov.br>
2023-03-23 20:18:43 -0300

    configure styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from "styled-components";
 import { DataProvider } from "./contexts/Context";
 import { Router } from "./contexts/Router";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { DefaultTheme } from "./styles/themes/DefaultTheme";
 
 export function App() {
 
   return (
     <DataProvider>
-      <Router />
+      <ThemeProvider theme={DefaultTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
     </DataProvider>
   )
 }
